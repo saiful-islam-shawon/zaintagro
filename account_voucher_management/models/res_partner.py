848b8021fcde10000145389deedc62a03fad68aa
saiful-islam-shawon <saifulislamshawon1234@gmail.com>
2026-09-21 14:58:35 +0600

    remove custom voucher module

--- account_voucher_management/models/res_partner.py
+++
@@ -1,8 +0,0 @@
-from odoo import fields, models
-
-
-class ResPartner(models.Model):
-    _inherit = "res.partner"
-
-    is_customer = fields.Boolean(string="Voucher Customer", tracking=True)
-    is_vendor = fields.Boolean(string="Voucher Vendor", tracking=True)
